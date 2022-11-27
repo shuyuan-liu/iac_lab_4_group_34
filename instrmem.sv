85c67df7ef0d54f0b992e4ef81d837004428bcf9
limbo-22 <105926205+limbo-22@users.noreply.github.com>
2022-11-27 23:30:11 +0000

    Update instrmem.sv

--- instrmem.sv
+++ instrmem.sv
@@ -6,7 +6,8 @@ module instrrom #(
     output logic[DATA_WIDTH-1:0] instr,
     output logic[4:0] rs2,
     output logic[4:0] rs1,
-    output logic[4:0] rd
+    output logic[4:0] rd,
+    output logic[11:0] immediate
 );
 
 program_counter program_counter (
@@ -21,6 +22,7 @@ rom instrrom(
 assign rs2 = instr[24:20];
 assign rs1 = instr[19:15];
 assign rd = instr[11:7];
+assign immediate = instr[31:20];
 
 
 endmodule 
