3a867824ecec75bc93356a9838f29da7a74ce663
limbo-22 <105926205+limbo-22@users.noreply.github.com>
2022-11-28 00:53:39 +0000

    Add files via upload

---
+++ sign_extend.sv
@@ -0,0 +1,24 @@
+module sign_extend #(
+    parameter WIDTH = 12
+)(
+    input logic[WIDTH-1:0] immediate,
+    input logic ImmSrc,
+    output logic [31:0] ImmOp
+);
+
+if(ImmSrc==1) begin
+    assign ImmOp = {{20{b[1]}},immediate};
+else
+    assign ImmOp = {{20{b[0]}},immediate};
+end
+
+instrmem immediate (
+    .immediate(immediate)
+);
+
+rom sign_extend(
+    .addr(immediate),
+    .dout(ImmOp)
+);
+
+endmodule
