ef91d282a0f2174a77a6d54814438007745a8997
limbo-22 <105926205+limbo-22@users.noreply.github.com>
2022-12-04 21:55:47 +0000

    Update sign_extend.sv

--- sign_extend.sv
+++ sign_extend.sv
@@ -7,13 +7,13 @@ module sign_extend (
 always_comb begin
     if(ImmSrc==0)  //I-type
         assign ImmExt = {{21{immediate[31]}},immediate[30:20]};
-    else if(ImmSrc==1)   //S-type
-        assign ImmExt = {{21{immediate[31]}},immediate[30:25],immediate[11:7]};
-    else if(ImmSrc==2)   //B-type
+    // else if(ImmSrc==1)   //S-type (not needed)
+        // assign ImmExt = {{21{immediate[31]}},immediate[30:25],immediate[11:7]};
+    else if(ImmSrc==1)   //B-type
         assign ImmExt = {{20{immediate[31]}},immediate[7],immediate[30:25],immediate[11:8],0};
-    else if(ImmSrc==3)   //U-type
+    else if(ImmSrc==2)   //U-type
         assign ImmExt = {immediate[31],immediate[30:20],immediate[19:12],{12{1'b0}}};
-    else if(ImmSrc==4)   //J-type
+    else if(ImmSrc==3)   //J-type
         assign ImmExt = {{12{immediate[31]}},immediate[19:12],immediate[20],immediate[30:25],immediate[24:21],1'b0};
 end
 
