614c76ed6c0f2f70cf86c910eefb85428bc0c6df
Shuyuan Liu <41851763+shuyuan-liu@users.noreply.github.com>
2022-12-08 12:37:19 +0000

    Upgrade ALU: more operationss, comparison outputs

--- alu/alu.sv
+++ alu/alu.sv
@@ -1,14 +1,29 @@
 module alu #(
     parameter DATA_WIDTH = 32
 )(
-    input  logic                 ctrl,
-    input  logic[DATA_WIDTH-1:0] op1,
-    input  logic[DATA_WIDTH-1:0] op2,
+    input  logic[2:0]            ctrl,
+    input  logic[DATA_WIDTH-1:0] op1, op2,
     output logic[DATA_WIDTH-1:0] out,
-    output logic                 zero
+    output logic                 zero, less_than_unsigned, less_than_signed
 );
 
-assign out = ctrl ? (op1 - op2) : (op1 + op2);
-assign zero = (out == 0);
+always_comb begin
+    case(ctrl)
+        0: out = op1 + op2;
+        1: out = op1 - op2;
+        2: out = op1 & op2;
+        3: out = op1 | op2;
+        4: out = op1 ^ op2;
+        5: out = op1 << op2;
+        6: out = op1 >> op2;
+        default: out = 0;
+    endcase
+end
+
+always_comb begin
+    zero = (out == 0);
+    unsigned_less_than = (unsigned'(op1) < unsigned'(op2));
+    signed_less_than = (signed'(op1) < signed'(op2));
+end
 
 endmodule
