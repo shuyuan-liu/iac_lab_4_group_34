e56000b8196564124a56c15cc8e392ae28abd0e8
Shuyuan Liu <41851763+shuyuan-liu@users.noreply.github.com>
2022-12-12 15:21:26 +0000

    Clean up testbench

--- f1_cpu/f1_cpu_tb.cpp
+++ f1_cpu/f1_cpu_tb.cpp
@@ -2,30 +2,25 @@
 #include "verilated_vcd_c.h"
 #include "Vf1_cpu.h"
 
-int main(int argc, char **argv, char **env) {
-    int i;
-    int clk;
-
+int main(int argc, char** argv) {
     Verilated::commandArgs(argc, argv);
-    Vf1_cpu* top = new Vf1_cpu;
+    auto top = new Vf1_cpu;
     Verilated::traceEverOn(true);
-    VerilatedVcdC* tfp = new VerilatedVcdC;
+    auto tfp = new VerilatedVcdC;
     top->trace(tfp, 99);
     tfp->open("f1_cpu.vcd");
 
-
-    for (i=0; i<10000; i++) {
-        top->clk = 1;
-        top->rst = 0;
-
-        for (clk=0;clk<2;clk++){
-            top->clk =! top->clk;
+    top->clk = 0;
+    for (int cycle = 0; cycle < 10000; cycle++) {
+        top->rst = (cycle == 0);
+        for (int tick = 0; tick < 2; tick++){
             top->eval();
-            tfp->dump (2*i+clk);
+            tfp->dump(2 * cycle + tick);
+            top->clk = !top->clk;
         }
-        
-        if(Verilated::gotFinish()) exit(0);
+        if (Verilated::gotFinish()) exit(0);
     }
+
     tfp->close();
     exit(0);
 }
