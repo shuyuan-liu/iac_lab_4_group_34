017d4ba8a1d13d8ec5a670ceb979c27f86f17f3c
limbo-22 <105926205+limbo-22@users.noreply.github.com>
2022-12-12 16:40:41 +0000

    Pipeline memory part

---
+++ pipeline/memory.sv
@@ -0,0 +1,23 @@
+module memory#(
+    parameter ADDR_WIDTH = 5,
+              DATA_WIDTH = 32
+)(
+    input logic clk,
+    input logic [DATA_WIDTH-1:0] ALUResultM,
+    input logic [DATA_WIDTH-1:0] WriteDataM,
+    input logic MemWriteM,
+    output logic [DATA_WIDTH-1:0] ReadDataM
+)
+
+ram Data_Memory(
+    .read_addr(ALUResultM),
+    .write_addr(ALUResultM),
+    .din(WriteDataM),
+    .dout(ReadDataM),
+    .write_en(MemWriteM),
+    .clk(clk)
+);
+
+
+
+endmodule
