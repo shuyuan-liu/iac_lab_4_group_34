2a2f5534eed12e60a32474f27a94f491002f7bf7
Shuyuan Liu <41851763+shuyuan-liu@users.noreply.github.com>
2022-12-12 14:23:28 +0000

    Automate making hex file

--- f1_cpu/doit.sh
+++ f1_cpu/doit.sh
@@ -1,8 +1,11 @@
 rm -rf obj_dir
 rm -f f1_cpu.vcd
 
-verilator --cc --trace --exe f1_cpu.sv register_file.sv  alu.sv control.sv mux_2.sv program_counter.sv register_file.sv instr_rom.sv sign_extend.sv ram.sv adder.sv f1_cpu_tb.cpp
+make -C ./src hexfile
+mv ./src/myprog/test.s.hex ./
 
-make -j -C obj_dir/ -f Vf1_cpu.mk 
+verilator --cc --trace --exe f1_cpu.sv register_file.sv alu.sv control.sv mux_2.sv program_counter.sv register_file.sv instr_rom.sv sign_extend.sv ram.sv f1_cpu_tb.cpp
+
+make -j -C obj_dir/ -f Vf1_cpu.mk
 
 obj_dir/Vf1_cpu
