187396a6d0d4d841d05796dc5bb875fd41dc2080
Shuyuan Liu <41851763+shuyuan-liu@users.noreply.github.com>
2022-12-15 19:39:30 +0000

    Keep copy of hex files in their original dir

--- f1_cpu/doit.sh
+++ f1_cpu/doit.sh
@@ -6,7 +6,7 @@ rm -rf obj_dir
 rm -f f1_cpu.vcd
 
 make -C ./src hexfile
-mv ./src/myprog/*.s.hex ./
+cp ./src/myprog/*.s.hex ./
 
 verilator --cc --trace --exe f1_cpu.sv register_file.sv alu.sv control.sv mux_2.sv program_counter.sv instr_rom.sv sign_extend.sv ram.sv f1_cpu_tb.cpp
 
