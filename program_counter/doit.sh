f32c4a8eeac256ab3d4d2d1b772b74498eb853d5
Shuyuan Liu <41851763+shuyuan-liu@users.noreply.github.com>
2022-12-16 20:10:15 +0000

    Clean up unneeded files before submission

--- program_counter/doit.sh
+++
@@ -1,8 +0,0 @@
-rm -rf obj_dir
-rm -f program_counter.vcd
-
-verilator -Wall --cc --trace program_counter.sv --exe ProgramCounter_tb.cpp
-
-make -j -C obj_dir/ -f Vprogram_counter.mk 
-
-obj_dir/Vprogram_counter
